b6c9a46c8afb8cecd2487a087e85c97835f8b514
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-05-14 07:20:36 +0530

    Token Type Definition

---
+++ litecode/src/TypesAndData/TokenType.cpp
@@ -0,0 +1,23 @@
+namespace toylang {
+        enum class TokenType {
+        
+        // Single character tokens.
+        LEFT_PARENTH, RIGHT_PARENTH, LEFT_BRACE, RIGHT_BRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+        // One or two character tokens.
+        BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER,
+        GREATER_EQUAL, LESS, LESS_EQUAL,
+
+        // Literals.
+        IDENTIFIER, STRING, NUMBER,
+
+        // Keywords.
+        AND, CLASS, ELSE, FALSE, FUN, FOR,
+        IF, NILL, OR, PRINT, RETURN, SUPRER,
+        THIS, TRUE, VAR, WHILE, 
+
+        END_OF_FILE
+
+    };
+}
