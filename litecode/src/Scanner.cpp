968b6ee81e87f8458883b065ff6341a41165b5fe
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-08-24 12:16:33 +0530

    Scanner or Lexer

--- litecode/src/Scanner.cpp
+++ litecode/src/Scanner.cpp
@@ -88,9 +88,76 @@ namespace toylang {
                     case ')' : return makeToken(TokenType::RIGHT_PARENTH);
                     case '{' : return makeToken(TokenType::LEFT_BRACE);
                     case '}' : return makeToken(TokenType::RIGHT_BRACE);
-                    
+                    case ',' : return makeToken(TokenType::COMMA);
+                    case '.' : return makeToken(TokenType::DOT);
+                    case '-' : return makeToken(TokenType::MINUS);
+                    case '+' : return makeToken(TokenType::PLUS);
+                    case ';' : return makeToken(TokenType::SEMICOLON);
+                    case '*' : return makeToken(TokenType::STAR);
+                    case '!' : return makeToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
+                    case '=' : return makeToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
+                    case '<' : return makeToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+                    case '>' : return makeToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
+                    case '/' : 
+                        if(match('/')) {
+                            while(peek() != '\n' && !isAtEnd())
+                                advance();
+                            return scanToken();
+                        }
+                        return makeToken(TokenType::SLASH);
+                    case '"' : return string();
+                    default  :
+                        if(std::isdigit(c))
+                            return number();
+                        if(std::isalpha(c) || c == '_')
+                            return identifier();
+                        std::cerr << "Unexpected character: " << c << '\n';
+                        return makeToken(TokenType::EOF);
                 }
             }
 
+            Token number() {
+                while(std::isdigit(peek()))
+                    advance();
+                return makeToken(TokenType::NUMBER, source.substr(start, current - start));
+            }
+
+            Token string() {
+                while(peek() != '"' && !isAtEnd()) {
+                    if(peek() == '\n')
+                        line++;
+                    advance();
+                }
+                if(isAtEnd()) {
+                    std::cerr << "Unexpected string!\n";
+                    return makeToken(TokenType::EOF);
+                }
+                advance();
+                return makeToken(TokenType::STRING, source.substr(start+1, current - start - 2));
+            }
+
+            Token identifier() {
+                while(std::isalnum(peek()) || peek() == '_')
+                    advance();
+                std::string text = source.substr(start, current - start);
+                TokenType type = Keywords.count(text) ? Keywords.at(text) : TokenType::IDENTIFIER;
+
+                return makeToken(type);
+            }
+
+            Toke makeToken(TokenType type, const std::string& literal = "") {
+                return Token(type, source.substr(start, current - start), literal, line);
+            }
+
+            static const std::unordered_map<std::string, TokenType> Keywords;
     };
+
+    const std::unordered_map<std::string, TokenType> Scanner::Keywords = {
+        {"and", TokenType::AND}, {"class", TokenType::CLASS}, {"else", TokenType::ELSE},
+        {"false", TokenType::FALSE}, {"for", TokenType::FOR}, {"fun", TokenType::FUN},
+        {"if", TokenType::IF}, {"nil", TokenType::NILL}, {"or", TokenType::OR},
+        {"print", TokenType::PRINT}, {"super", TokenType::SUPER}, {"return", TokenType::RETURN},
+        {"this", TokenType::THIS}, {"var", TokenType::VAR}, {"true", TokenType::TRUE},
+        {"while", TokenType::WHILE}
+    }
 }
