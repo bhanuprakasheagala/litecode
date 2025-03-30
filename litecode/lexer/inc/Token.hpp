82c3a73807cd4ee068619db2fc895eecc6b77d1d
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2025-03-30 15:46:19 +0530

    Trim leading whitespaces while collecting the lexemes

--- litecode/lexer/inc/Token.hpp
+++ litecode/lexer/inc/Token.hpp
@@ -14,7 +14,7 @@ namespace lexer {
 
             friend std::ostream& operator<<(std::ostream& os, const Token& token) {
                 os << "[" << toString(token.type) << "] "
-                << "Lexeme: \"" << token.lexeme << "\" "
+                << "Lexeme: \"" << trim(token.lexeme) << "\" "
                 << "Literal: \"" << token.literal << "\" "
                 << "Line: " << token.line;
                 return os;
@@ -25,6 +25,13 @@ namespace lexer {
             const std::string lexeme;
             const std::string literal;
             const int line;
+
+            // Helper function to trim leading and trailing whitespace
+            static std::string trim(const std::string& str) {
+                auto start = std::find_if_not(str.begin(), str.end(), ::isspace);
+                auto end = std::find_if_not(str.rbegin(), str.rend(), ::isspace).base();
+                return (start < end) ? std::string(start, end) : "";
+            }
     };
 }
 
