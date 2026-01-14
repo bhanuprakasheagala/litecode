81cd27ab6603a973b14d34bcd414b4ac120c1eed
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2026-01-14 22:30:56 +0530

    Expression Tree

---
+++ litecode/parser/inc/Expr.hpp
@@ -0,0 +1,51 @@
+#ifndef EXPR_HPP
+#define EXPR_HPP
+
+#include <memory>
+#include <variant>
+#include "lexer/inc/Token.hpp"
+
+namespace parser {
+
+    struct Expr {
+        virtual ~Expr() = default;
+    };
+
+    using ExprPtr = std::unique_ptr<Expr>;
+
+    // ===== Expression Types =====
+
+    struct Binary : Expr {
+        ExprPtr left;
+        lexer::Token op;
+        ExprPtr right;
+
+        Binary(ExprPtr left, lexer::Token op, ExprPtr right)
+            : left(std::move(left)), op(std::move(op)), right(std::move(right)) {}
+    };
+
+    struct Unary : Expr {
+        lexer::Token op;
+        ExprPtr right;
+
+        Unary(lexer::Token op, ExprPtr right)
+            : op(std::move(op)), right(std::move(right)) {}
+    };
+
+    struct Literal : Expr {
+        std::variant<std::nullptr_t, bool, double, std::string> value;
+
+        explicit Literal(std::variant<std::nullptr_t, bool, double, std::string> value)
+            : value(std::move(value)) {}
+    };
+
+    struct Grouping : Expr {
+        ExprPtr expression;
+
+        explicit Grouping(ExprPtr expression)
+            : expression(std::move(expression)) {}
+    };
+
+} // namespace parser
+
+#endif // EXPR_HPP
